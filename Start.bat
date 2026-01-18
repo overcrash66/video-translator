72f0932f2ada7a0384e3e3445b1d14ed4ba43526
overcrash <3681221+overcrash66@users.noreply.github.com>
2026-01-17 21:37:58 -0400

    Create Start.bat

---
+++ Start.bat
@@ -0,0 +1,12 @@
+@echo off
+set CURRENT_DIR=%CD%
+echo ***** Current directory: %CURRENT_DIR% *****
+set PYTHONPATH=%CURRENT_DIR%
+
+rem Activate Python virtual environment if exists
+if exist "venv\Scripts\activate.bat" (
+    call venv\Scripts\activate.bat
+)
+
+python app.py
+pause
